ba63697338bc3a9a7c17f66db67ac5231292b029
JaredLove <lovejared91@Yahoo.com>
2023-09-06 18:54:51 -0500

    added stripe

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from './pages/OrderHistory';
+import Success from './pages/Success';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -50,6 +51,10 @@ function App() {
             <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
+            <Route
+            path="/success"
+            element={<Success />}
+          />
           </Switch>
           </StoreProvider>
         </div>
